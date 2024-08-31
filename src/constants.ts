369d37a8e6072dde910a314b714ddcb33992f7d4
tris <nexus4fortune@gmail.com>
2024-08-31 20:49:27 +0900

    feat: トップページで複数カテゴリーに対応

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,20 @@
 export const SITE_TITLE = "TRISHAFT";
+
+export type Categories = "typescript" | "rust";
+
+/** カテゴリーの表示情報 */
+export type CategoryInfo = {
+  /** カテゴリー */
+  category: Categories;
+  /** カテゴリーの表示文字列 */
+  title: string;
+};
+
+/** カテゴリーを表示順に並べた配列 */
+export const CATEGORIES_LIST: Categories[] = ["typescript", "rust"];
+
+/** カテゴリーの情報 */
+export const CATEGORIES_DATA: Record<Categories, CategoryInfo> = {
+  typescript: { category: "typescript", title: "TypeScript" },
+  rust: { category: "rust", title: "Rust" },
+};
---
+++ src/content/rust/ts-test.md
@@ -0,0 +1,26 @@
+---
+title: Rust の記事のテスト
+pubDate: 2020-01-30
+publish: true
+---
+
+# テスト！
+
+これは Rust に関するテストのための記事。
+
+ここを**太字にしたり**する。
+
+```ts
+function add(a:number, b:number):number {
+  return a + b;
+}
+
+const v = add(1 + 2);
+```
+
+- リスト
+  - を書いて
+- みた。
+
+最後。
+
